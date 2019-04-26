62e7f0afa73d2cfba2eed8cef8742d8ad621f8f7
Techgawd <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2019-04-25 22:50:58 -0400

    Cleaned up class constructors, added super constructors and some objects

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,53 +11,116 @@ class Person {
         }
     }
 
-    class Instructor extends Person {
-        constructor (attributes){
-            this.specialty = attributes.specialty;
-            this.favLanguage = attributes.favLanguage;
-            this.catchPhrase = attributes.catchPhrase;
-            this.gender = attributes.gender;
-        }
-            demo(subject){
-                return `Today we learned about ${this.subject}`;
+class Instructor extends Person {
+    constructor (attributes){
+        super (attributes);
+        this.specialty = attributes.specialty;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+    }
+        demo(subject){
+            return `Today we learned about ${this.subject}`;
             }
 
-            grade(student, subject){
-                return `${this.student} receives a perfect score on ${this.subject}`;
-
+        grade(student, subject){
+            return `${this.student} receives a perfect score on ${this.subject}`;
             }
         }   
+    
         
-     class Student extends Instructor {
-            constructor (attributes){
-                this.previousBackground = attributes.previousBackground;
-                this.className = attributes.className;
-                this.favSubjects = attributes.favSubjects;
+class Student extends Instructor {
+    constructor (attributes){
+        super (attributes);
+        this.previousBackground = attributes.previousBackground;
+        this.className = attributes.className;
+        this.favSubjects = attributes.favSubjects;
+    }
+        listsSubjects(){
+            return `Today we learned about ${this.favSubjects}`;
             }
-                listsSubjects(){
-                    return `Today we learned about ${this.subject}`;
-                }
     
-                PRAssignment(subject){
-                    return `${student.name} has submitted a PR for ${this.subject}`;
-                }
+        PRAssignment(subject){
+            return `${student.name} has submitted a PR for ${this.subject}`;
+            }
 
-                sprintChallenge(subject){
-                    return `${student.name} has begun sprint challenge on ${this.subject}`;
-                }
-            } 
+        sprintChallenge(subject){
+            return `${student.name} has begun sprint challenge on ${this.subject}`;
+            }
+        } 
 
-            class ProjectManager extends Student {
-                constructor (attributes){
-                    this.gradClassName = attributes.gradClassName;
-                    this.favInstructor = attributes.className;
+class ProjectManager extends Student {
+    constructor (attributes){
+        super (attributes);
+        this.gradClassName = attributes.gradClassName;
+        this.favInstructor = attributes.className;
                 }
-                    standUp(channel){
-                        return `${this.name} announces to ${this.channel}, @channel standy times!`;
-                    }
+        standUp(channel){
+            return `${this.name} announces to ${this.channel}, @channel standy times!`;
+            }
         
-                    debugsCode(subject){
-                        return `${student.name} debugs ${student.name}'s code on ${this.subject}`;
-                    }
-                } 
+        debugsCode(subject){
+            return `${student.name} debugs ${student.name}'s code on ${this.subject}`;
+            }
+        } 
     
+const ben = new Instructor( {
+    "name": "Ben",
+    "age": "31",
+    "location": "Florida",
+    "gender": "Male",
+    "specialty": "Javascript",
+    "favLanguage": "React",
+    "catchPhrase": "That's pretty cool"
+});
+
+const mary = new Instructor( {
+    "name": "Ben",
+    "age": "31",
+    "location": "Florida",
+    "gender": "Male",
+    "specialty": "Javascript",
+    "favLanguage": "React",
+    "catchPhrase": "That's pretty cool"
+});
+
+const susan = new Student( {
+    "name": "Ben",
+    "age": "31",
+    "location": "Florida",
+    "gender": "Male",
+    "previousBackground": "Retail",
+    "className": "PT7",
+    "favSubjects": ["css","html","Javascript"]
+});
+
+const bill = new Student( {
+    "name": "Ben",
+    "age": "31",
+    "location": "Florida",
+    "gender": "Male",
+    "previousBackground": "Retail",
+    "className": "PT7",
+    "favSubjects": ["css","html","Javascript"]
+});
+
+const thomas = new ProjectManager( {
+    "name": "Ben",
+    "age": "31",
+    "location": "Florida",
+    "gender": "Male",
+    "specialty": "Javascript",
+    "favLanguage": "React",
+    "catchPhrase": "That's pretty cool"
+});
+
+const mike = new ProjectManager( {
+    "name": "Ben",
+    "age": "31",
+    "location": "Florida",
+    "gender": "Male",
+    "specialty": "Javascript",
+    "favLanguage": "React",
+    "catchPhrase": "That's pretty cool"
+});
+
+console.log (susan.listsSubjects());
